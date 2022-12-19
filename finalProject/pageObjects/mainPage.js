e3acf07f593f2f750759c59d5ecddc82a4cba7e3
pavelkhalkov <pavel.khalkov@mail.ru>
2022-12-19 19:29:00 +0300

    finalProject

---
+++ finalProject/pageObjects/mainPage.js
@@ -0,0 +1,37 @@
+const BasePage = require('./BasePage');
+
+class MainPage extends BasePage {
+  get menSectionBanner() {
+    return '.js-ibanner-bg-image:nth-child(3)';
+  }
+
+  get generalBrand() {
+    return '.main-trend-style__wrapper';
+  }
+
+  get mobileApplic() {
+    return '.mobile-bannerii__container.js-ibanner-bg-image';
+  }
+
+  get menGoodsButton() {
+    return '._shadow_rywrs_6';
+  }
+
+  get mainBreadcrumbsButton() {
+    return '.swiper-slide._slide_ul8d6_13.swiper-slide-active';
+  }
+
+  get titleOfBrand() {
+    return '._titleText_641wy_15.ui-catalog-search-head-title';
+  }
+
+  get windowLoginArea() {
+    return '.d-modal__content_paddings.d-modal__content_default';
+  }
+
+  get emptySearchWindow() {
+    return '//h1[@class="_title_ny4g9_24"]';
+  }
+}
+
+module.exports = MainPage;
---
+++ finalProject/tests/navigation.spec.js
@@ -0,0 +1,49 @@
+const { test, expect } = require('@playwright/test');
+
+const PageFactory = require('../pageObjects/pageFactory.js');
+const BaseElements = require('../helpers/baseElements');
+
+test.describe('Test scenarios for Lamoda application to check the navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    const p = new PageFactory(page);
+    await p.basePage.navigate('https://www.lamoda.ru/');
+  });
+
+  test('Check that main page is opened after clicking Lamoda label at the top of the page', async ({
+    page,
+  }) => {
+    const element = new PageFactory(page);
+    const I = new BaseElements(page);
+    await I.click(element.mainPage.menSectionBanner);
+    await I.click(element.header.sportMenubarTab);
+    await I.click(element.productsPage.clothesForHall);
+    await I.click(element.header.lamodaLogo);
+    await expect(page).toHaveURL('https://www.lamoda.ru/men-home/');
+  });
+
+  test('Check that a particular section can be opened from the footer ', async ({
+    page,
+  }) => {
+    const element = new PageFactory(page);
+    const I = new BaseElements(page);
+    await I.click(element.footer.menFooterButton);
+    await I.click(element.footer.clothesFooterButton);
+    await expect(page.locator(element.productsPage.titleOfBrand)).toContainText(
+      'Мужская одежда',
+    );
+    await expect(page).toHaveURL(
+      'https://www.lamoda.ru/c/477/clothes-muzhskaya-odezhda/',
+    );
+  });
+
+  test('Check that page can be switched to the main page using breadcrumbs', async ({
+    page,
+  }) => {
+    const element = new PageFactory(page);
+    const I = new BaseElements(page);
+    await I.click(element.header.menSection);
+    await I.click(element.header.clothesMenubarTab);
+    await I.click(element.mainPage.mainBreadcrumbsButton);
+    await expect(page).toHaveURL('https://www.lamoda.ru/');
+  });
+});
